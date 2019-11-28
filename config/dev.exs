4064ab6a7e1cbe59539adb495bf42b96cc2bc2f1
Lluis Toyos <ltoyos@gmail.com>
2019-11-28 18:53:18 +0100

    deactivate debug_errors on DEV

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :rumbl, Rumbl.Repo,
 # with webpack to recompile .js and .css sources.
 config :rumbl, RumblWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: [
