761231766ad929d0adf4dfe4e0d9b3f769d50781
Lluis Toyos <ltoyos@gmail.com>
2019-11-29 19:28:20 +0100

    CH5.P93 Logging In

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -24,6 +24,7 @@ defmodule RumblWeb.UserController do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
