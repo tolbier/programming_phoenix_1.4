fedf96ab1f597693a8799dccff53d27ba82dc812
Lluis Toyos <ltoyos@gmail.com>
2019-11-29 20:01:52 +0100

    CH5.P97 Presenting User Account Links

--- lib/rumbl_web/controllers/session_controller.ex
+++ lib/rumbl_web/controllers/session_controller.ex
@@ -19,4 +19,10 @@ defmodule RumblWeb.SessionController do
         |> render("new.html")
     end
   end
+
+  def delete(conn, _) do
+    conn
+    |> RumblWeb.Auth.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
 end
