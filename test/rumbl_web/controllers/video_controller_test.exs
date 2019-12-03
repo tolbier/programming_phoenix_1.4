1a34c579f8e55fcf162d3ff50f483363e838ba33
Lluis Toyos <ltoyos@gmail.com>
2019-12-03 12:12:03 +0100

    CH8.P163 Testing Logged-In Users

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -1,5 +1,25 @@
 defmodule RumblWeb.VideoControllerTest do
   use RumblWeb.ConnCase, async: true
+
+  setup %{conn: conn, login_as: username} do
+    user = user_fixture(username: username)
+    conn = assign(conn, :current_user, user)
+
+    {:ok, conn: conn, user: user}
+  end
+
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_video  = video_fixture(user, title: "funny cats")
+    other_video = video_fixture(
+      user_fixture(username: "other"),
+      title: "another video")
+
+    conn = get conn, Routes.video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing Videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
+  end
+
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
       get(conn, Routes.video_path(conn, :new)),
